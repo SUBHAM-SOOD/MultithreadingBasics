724578f182d8bdc5adf16c6d80b1c58a3fa189b6
Subham <sood1212subham@gmail.com>
2021-01-06 23:21:10 +0530

    Using some Thread Methods

---
+++ Mutlithreading/Mt_07.java
@@ -0,0 +1,44 @@
+package Mutlithreading;
+
+class Sp1 extends Thread{
+    @Override
+    public void run(){
+        while(true){
+            try{
+                System.out.println("Hi from Sood "+getName());
+                Thread.sleep(500);
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+        }
+    }
+}
+class Sp2 extends Thread{
+    @Override
+    public void run(){
+        while(true){
+            try{
+                System.out.println("I am from second class "+getName());
+                Thread.sleep(500);
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class Mt_07 {
+
+    public static void main(String args[]){
+        Sp1 nAbc1 = new Sp1();
+        Sp2 nAbc2 = new Sp2();
+        nAbc1.setPriority(Thread.MAX_PRIORITY);
+        nAbc2.setPriority(Thread.MIN_PRIORITY);
+        nAbc1.setName("TH01");
+        nAbc2.setName("TH02");
+        nAbc1.start();
+        nAbc2.start();
+    }
+}
